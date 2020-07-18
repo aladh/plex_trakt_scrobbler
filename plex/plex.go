b01efa4b40001ab14ac372e5dc3e367b577a63f2
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-07-18 13:41:40 -0400

    Move hardcoded string to constant

--- plex/plex.go
+++ plex/plex.go
@@ -2,6 +2,8 @@ package plex
 
 import "log"
 
+const scrobbleEvent = "media.scrobble"
+
 func ShouldProcess(payload *Payload, serverUUID string) bool {
 	// Check that the webhook is coming from the right server
 	if payload.Server.UUID != serverUUID {
@@ -10,7 +12,7 @@ func ShouldProcess(payload *Payload, serverUUID string) bool {
 	}
 
 	// Only send watch request when media has been completely watched
-	if payload.Event != "media.scrobble" {
+	if payload.Event != scrobbleEvent {
 		return false
 	}
 
