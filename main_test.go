8cd4d71fd653d3f7a1014b2b4cf45582d9e47f97
Alishan Ladhani <git@alad.ca>
2021-08-02 12:54:13 -0400

    Create end-to-end test
    Does not clean up the added movie yet

---
+++ main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestWatchMovieWebhook(t *testing.T) {
+	_, enabled := os.LookupEnv("ENABLE_E2E_TESTS")
+	if !enabled {
+		t.Skip("End to end tests are not enabled")
+	}
+
+	// Start server
+	go main()
+
+	payload, err := os.ReadFile("testdata/webhook.json")
+	if err != nil {
+		t.Fatalf("error opening fixture file: %s", err)
+	}
+
+	var formBuffer bytes.Buffer
+	multipartWriter := multipart.NewWriter(&formBuffer)
+	payloadWriter, err := multipartWriter.CreateFormField("payload")
+	if err != nil {
+		t.Fatalf("error creating form field: %s", err)
+	}
+
+	_, err = payloadWriter.Write(payload)
+	if err != nil {
+		t.Fatalf("error writing form field: %s", err)
+	}
+
+	err = multipartWriter.Close()
+	if err != nil {
+		t.Fatalf("error closing form writer: %s", err)
+	}
+
+	response, err := http.Post("http://localhost:3000", multipartWriter.FormDataContentType(), &formBuffer)
+	if err != nil {
+		t.Fatalf("error making request to app server: %s", err)
+	}
+
+	if response.StatusCode != 200 {
+		t.Fatalf("bad response from app server")
+	}
+}
