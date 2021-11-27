6c9616a70ca0537888454222314316b1194dda82
Alishan Ladhani <git@alad.ca>
2021-11-27 13:17:14 -0500

    Refactor error handling

--- server/server.go
+++ server/server.go
@@ -14,30 +14,12 @@ import (
 
 const postMethod = "POST"
 
-func Handler(cfg *config.Config) func(writer http.ResponseWriter, request *http.Request) {
+func Handler(cfg *config.Config) func(http.ResponseWriter, *http.Request) {
 	traktClient := trakt.New(cfg.TraktClientID, cfg.TraktClientSecret, cfg.TraktAccessToken)
 
-	return func(writer http.ResponseWriter, request *http.Request) {
-		// Plex webhooks are always POST
-		if request.Method != postMethod {
-			return
-		}
-
-		payload, err := parsePayload(request)
+	return func(_ http.ResponseWriter, request *http.Request) {
+		err := processRequest(cfg, traktClient, request)
 		if err != nil {
-			log.Printf("Error parsing webhook payload: %s\n", err)
-			return
-		}
-
-		if !plex.ShouldProcess(payload, cfg.PlexServerUUIDs, cfg.PlexUsername) {
-			return
-		}
-
-		log.Printf("Received scrobble: %v\n", payload)
-
-		err = watchMedia(payload, traktClient)
-		if err != nil {
-			err = fmt.Errorf("error watching media: %w", err)
 			log.Println(err)
 			log.Printf("Request payload: %s\n", request.FormValue("payload"))
 
@@ -47,6 +29,31 @@ func Handler(cfg *config.Config) func(writer http.ResponseWriter, request *http.
 			}
 		}
 	}
+}
+
+func processRequest(cfg *config.Config, traktClient *trakt.Trakt, request *http.Request) error {
+	// Plex webhooks are always POST
+	if request.Method != postMethod {
+		return nil
+	}
+
+	payload, err := parsePayload(request)
+	if err != nil {
+		return fmt.Errorf("error parsing webhook payload: %w", err)
+	}
+
+	if !plex.ShouldProcess(payload, cfg.PlexServerUUIDs, cfg.PlexUsername) {
+		return nil
+	}
+
+	log.Printf("Received scrobble: %v\n", payload)
+
+	err = watchMedia(payload, traktClient)
+	if err != nil {
+		return fmt.Errorf("error watching media: %w", err)
+	}
+
+	return nil
 }
 
 func parsePayload(request *http.Request) (*plex.Payload, error) {
